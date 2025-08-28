12e067466f22af3df04ee36cd0642fc8f80cf9a5
smarios <smarios@jaist.ac.jp>
2025-08-28 16:59:47 +0900

    config timeout/planner_id, move randomly and with poses

--- plan_example_python/motion_planning_python_api_tutorial.py
+++ plan_example_python/motion_planning_python_api_tutorial.py
@@ -45,11 +45,15 @@ def plan_and_execute(
         logger.info("Executing plan")
         robot_trajectory = plan_result.trajectory
         robot.execute(robot_trajectory, controllers=[])
+        logger.info("Execute finished!")
     else:
         logger.error("Planning failed")
 
     time.sleep(sleep_time)
 
+def log_positions(robot_state, logger):
+    positions = robot_state.get_joint_group_positions("lite6")
+    logger.info(f"positions: {positions}")
 
 def main():
 
@@ -58,7 +62,7 @@ def main():
     ###################################################################
     # marios
     # just to make sure everything is set up before we start doing things
-    time.sleep(10)
+    time.sleep(5)
     rclpy.init()
     logger = get_logger("moveit_py.pose_goal")
 
@@ -66,6 +70,8 @@ def main():
     cobot = MoveItPy(node_name="moveit_py")
     arm = cobot.get_planning_component("lite6")
     logger.info("MoveItPy instance created")
+    time.sleep(5)
+    logger.info("Sleeping for 5 secs...")
 
     ###########################################################################
     # Plan 1 - set states with predefined string
@@ -78,52 +84,60 @@ def main():
     arm.set_goal_state(configuration_name="home")
 
     # plan to goal
-    plan_and_execute(cobot, arm, logger, sleep_time=8.0)
+    plan_and_execute(cobot, arm, logger, sleep_time=1.0)
 
     ############################################################################
     ## Plan 2 - set goal state with RobotState object
     ############################################################################
 
     ## instantiate a RobotState instance using the current robot model
-    #robot_model = cobot.get_robot_model()
-    #robot_state = RobotState(robot_model)
+    robot_model = cobot.get_robot_model()
+    robot_state = RobotState(robot_model)
+    log_positions(robot_state, logger)
 
-    ## randomize the robot state
-    #robot_state.set_to_random_positions()
 
-    ## set plan start state to current state
-    #arm.set_start_state_to_current_state()
+    # randomize the robot state
+    robot_state.set_to_random_positions()
 
-    ## set goal state to the initialized robot state
-    #logger.info("Set goal state to the initialized robot state")
-    #arm.set_goal_state(robot_state=robot_state)
+    # set plan start state to current state
+    arm.set_start_state_to_current_state()
 
-    ## plan to goal
-    #plan_and_execute(cobot, arm, logger, sleep_time=8.0)
+    # set goal state to the initialized robot state
+    logger.info("Set goal state to the initialized robot state")
+    arm.set_goal_state(robot_state=robot_state)
 
-    ############################################################################
-    ## Plan 3 - set goal state with PoseStamped message
-    ############################################################################
-    ## TODO figure out the coordinate system
+    # plan to goal
+    plan_and_execute(cobot, arm, logger)
 
-    ## set plan start state to current state
-    #arm.set_start_state_to_current_state()
+    log_positions(robot_state, logger)
 
-    ## set pose goal with PoseStamped message
-    #from geometry_msgs.msg import PoseStamped
+    ###########################################################################
+    # Plan 3 - set goal state with PoseStamped message
+    ###########################################################################
+    # TODO figure out the coordinate system
 
-    #pose_goal = PoseStamped()
-    #pose_goal.header.frame_id = "link_base"
-    #pose_goal.pose.orientation.w = 1.0
-    #pose_goal.pose.position.x = 10.0
-    #pose_goal.pose.position.y = 0.0
-    #pose_goal.pose.position.z = 10.0
-    #arm.set_goal_state(
-    #    # that or joint6_flange maybe?
-    #    pose_stamped_msg=pose_goal, pose_link="link6")
+    # set plan start state to current state
+    arm.set_start_state_to_current_state()
 
-    ## plan to goal
-    #plan_and_execute(cobot, arm, logger, sleep_time=8.0)
+    # set pose goal with PoseStamped message
+    from geometry_msgs.msg import PoseStamped
+
+    pose_goal = PoseStamped()
+    pose_goal.header.frame_id = "link_base"
+    pose_goal.pose.orientation.w = 1.0
+    pose_goal.pose.orientation.x = 1.0
+    #pose_goal.pose.orientation.y = .5
+    #pose_goal.pose.orientation.z = -1.0
+    #these are METERS! don't push your luck
+    pose_goal.pose.position.x = 0.2
+    pose_goal.pose.position.y = 0.0
+    pose_goal.pose.position.z = 0.4
+    arm.set_goal_state(
+        # that or joint6_flange maybe?
+        pose_stamped_msg=pose_goal, pose_link="link6")
+
+    # plan to goal
+    plan_and_execute(cobot, arm, logger, sleep_time=8.0)
 
     ############################################################################
     ## Plan 4 - set goal state with constraints
@@ -136,12 +150,12 @@ def main():
     #from moveit.core.kinematic_constraints import construct_joint_constraint
 
     #joint_values = {
-    #    "joint1": -55.4,
-    #    "joint2": 27.0,
-    #    "joint3": 45.0,
-    #    "joint4": 12.0,
-    #    "joint5": 17.0,
-    #    "joint6": -12.8,
+    #    "joint1": -55.4/180.,
+    #    "joint2": 27.0/180.,
+    #    "joint3": 45.0/180.,
+    #    "joint4": 12.0/180.,
+    #    "joint5": 17.0/180.,
+    #    "joint6": -12.8/180,
     #    }
 
     #robot_model = cobot.get_robot_model()
@@ -156,6 +170,8 @@ def main():
 
     ## plan to goal
     #plan_and_execute(cobot, arm, logger, sleep_time=8.0)
+
+    #log_positions(robot_state, logger)
 
     ############################################################################
     ## Plan 5 - Planning with Multiple Pipelines simultaneously
