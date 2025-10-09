c16db56d12ec54dea28ba99aa4afdc276efbac68
smarios <smarios@jaist.ac.jp>
2025-10-09 16:04:40 +0900

    Remove comments from tutorial launch file
    Co-authored-by: Jawad Khan <jawadkhan.tech@gmail.com>

--- launch/motion_planning_python_api_tutorial.launch.py
+++ launch/motion_planning_python_api_tutorial.launch.py
@@ -22,7 +22,6 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    #TODO include lite6_moveit_real launch file        moveit_py_node,
     robot_ip = LaunchConfiguration('robot_ip')
     hw_ns = LaunchConfiguration('hw_ns', default='ufactory')
 
@@ -73,79 +72,10 @@ def generate_launch_description():
         parameters=[moveit_config.to_dict()],
         )
 
-    #rviz_config_file = os.path.join(
-    #    get_package_share_directory("plan_example_python"),
-    #    "config",
-    #    "moveit.rviz",
-    #    )
-
-    #rviz_node = Node(
-    #    package="rviz2",
-    #    executable="rviz2",
-    #    output="log",
-    #    arguments=["-d", rviz_config_file],
-    #    parameters=[
-    #        moveit_config.robot_description,
-    #        moveit_config.robot_description_semantic,
-    #        ],
-    #    )
-
-    #static_tf = Node(
-    #    package="tf2_ros",
-    #    executable="static_transform_publisher",
-    #    name="static_transform_publisher",
-    #    output="log",
-    #    arguments=["--frame-id", "world", "--child-frame-id", "link_base"],
-    #    )
-
-    #robot_state_publisher = Node(
-    #    package="robot_state_publisher",
-    #    executable="robot_state_publisher",
-    #    name="robot_state_publisher",
-    #    output="log",
-    #    parameters=[moveit_config.robot_description],
-    #    )
-
-    #ros2_controllers_path = os.path.join(
-    #    get_package_share_directory("plan_example_python"),
-    #    "config",
-    #    "ros2_controllers.yaml",
-    #    )
-    #ros2_control_node = Node(
-    #    package="controller_manager",
-    #    executable="ros2_control_node",
-    #    parameters=[ros2_controllers_path],
-    #    remappings=[
-    #        ("/controller_manager/robot_description", "/robot_description"),
-    #        ],
-    #    output="log",
-    #    )
-
-    #load_controllers = []
-    #for controller in [
-    #        #"arm_controller",
-    #        #"gripper_action_controller",
-    #        "lite6_traj_controller",
-    #        "joint_state_broadcaster",
-    #        ]:
-    #    load_controllers += [
-    #        ExecuteProcess(
-    #            cmd=["ros2 run controller_manager spawner {}".format(
-    #                controller)],
-    #            shell=True,
-    #            output="log",
-    #            )
-    #        ]
-
     return LaunchDescription(
         [
             example_file,
             moveit_py_node,
             lite6_real_launch,
-    ##        robot_state_publisher,
-    ##        ros2_control_node,
-    ##        rviz_node,
-    ##        static_tf,
             ]
-    ##    + load_controllers
         )
