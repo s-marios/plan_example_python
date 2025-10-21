e68064e1c375d96f684bae7dcc02570e13074ec5
smarios <smarios@jaist.ac.jp>
2025-10-21 15:03:29 +0900

    Add configurable robot logic, support vacuum gripper
    Co-authored-by: Jawad Khan <jawadkhan.tech@gmail.com>

--- plan_example_python/pick_place.py
+++ plan_example_python/pick_place.py
@@ -28,6 +28,9 @@ from geometry_msgs.msg import Pose, PoseStamped
 from moveit_msgs.msg import CollisionObject, AttachedCollisionObject, PlanningScene
 from shape_msgs.msg import SolidPrimitive
 
+# xarm messages
+from xarm_msgs.srv import VacuumGripperCtrl
+
 
 def euler_to_quaternion(yaw, pitch, roll):
     """
@@ -47,53 +50,55 @@ def log_positions(robot_state, logger):
     positions = robot_state.get_joint_group_positions("lite6")
     logger.info(f"positions: {positions}")
 
-class PickAndPlace():
+class RobotLogic():
 
-    def __init__(self):
-        rclpy.init()
-        self.logger = get_logger("pick_place")
+    def __init__(self, moveit, logger, pose_link):
+        self.moveit = moveit
+        self.logger = logger
+        self.pose_link = pose_link
 
-        # instantiate MoveItPy instance and get planning component
-        self.moveit = MoveItPy(node_name="pick_place")
-        self.arm = self.moveit.get_planning_component("lite6")
-        self.logger.info("Pick and Place MoveItPy instance created")
+    def attach_physical_object(self, obj):
+        pass
 
-        ### Setup a second node
-        self.node = rclpy.create_node("object_despawner")
-        self.logger.info("created object despawn node!")
+    def detach_physical_object(self):
+        pass
 
-        ## Setup necessary parameters
-        ## vacuum gripper
-        self.node.declare_parameter("add_vacuum_gripper", False)
-        self.vacuum_gripper = self.node.get_parameter("add_vacuum_gripper").get_parameter_value().bool_value
+    def move_to_object(self, obj):
+        pass
 
-        ## robot_ip
-        self.node.declare_parameter("robot_ip", "fake")
-        self.robot_ip = self.node.get_parameter("robot_ip").get_parameter_value().string_value
+    def move_to_drop_location(self, location):
+        pass
 
-        ## Setup necessary pub/sub
-        self.publisher = self.node.create_publisher(PlanningScene, "/planning_scene", 10)
-        self.subscriber = self.node.create_subscription(
-                PlanningScene,
-                "/planning_scene",
-                self.scene_changed_callback,
-                10)
+    def plan_and_execute(
+            self,
+            single_plan_parameters=None,
+            multi_plan_parameters=None,
+            sleep_time=0.0,
+            ):
+        """Helper function to plan and execute a motion."""
+        # plan to goal
+        self.logger.info("Planning trajectory")
+        if multi_plan_parameters is not None:
+            plan_result = self.arm.plan(
+                multi_plan_parameters=multi_plan_parameters
+                )
+        elif single_plan_parameters is not None:
+            plan_result = self.arm.plan(
+                single_plan_parameters=single_plan_parameters
+                )
+        else:
+            plan_result = self.arm.plan()
 
-        self.object_queue = SimpleQueue()
+        # execute the plan
+        if plan_result:
+            self.logger.info("Executing plan")
+            robot_trajectory = plan_result.trajectory
+            self.moveit.execute(robot_trajectory, controllers=[])
+            self.logger.info("Execute finished!")
+        else:
+            self.logger.error("Planning failed")
 
-    def start_spinning(self):
-        self.thread = threading.Thread(target=self.spin)
-        self.thread.start()
-
-    def spin(self):
-        self.logger.info("sub_thread: start spinning!")
-        rclpy.spin(self.node)
-
-
-    def scene_changed_callback(self, msg: PlanningScene):
-        self.logger.info("scene changed!")
-        for obj in extract_objects_from_scene_message(msg):
-            self.object_queue.put(obj)
+        time.sleep(sleep_time)
 
     def move_to(self, obj_pose: Pose):
         self.logger.info(f"object coordinates: {
@@ -116,12 +121,123 @@ class PickAndPlace():
         pose_goal.pose.position.y = obj_pose.position.y
         pose_goal.pose.position.z = obj_pose.position.z
 
-        self.arm.set_goal_state(pose_stamped_msg=pose_goal, pose_link="link6")
+        self.arm.set_goal_state(pose_stamped_msg=pose_goal, pose_link=self.pose_link)
         self.plan_and_execute()
+
+
+
+class RobotWithoutGripper(RobotLogic):
+
+    def __init__(self, moveit, logger, pose_link):
+         super().__init__(moveit, logger, pose_link)
+         self.arm = self.moveit.get_planning_component("lite6")
+
+    def move_to_object(self, obj_pose: Pose):
+         self.move_to(obj_pose)
+
+    def move_to_drop_location(self, location: Pose):
+        self.move_to(location)
+
+class RobotWithVacuumGripper(RobotLogic):
+
+    def __init__(self, moveit, logger, srv_cli):
+         super().__init__(moveit, logger, "link_eef")
+         self.cli = srv_cli
+         self.arm = self.moveit.get_planning_component("lite6")
+
+
+    def attach_physical_object(self, obj):
+        req = VacuumGripperCtrl.Request()
+        req.on = True
+        req.wait = True
+        res = self.cli.call(req)
+        #TODO: check result
+
+    def detach_physical_object(self):
+        req = VacuumGripperCtrl.Request()
+        req.on = False
+        req.wait = True
+        res = self.cli.call(req)
+
+    def move_to_object(self, obj_pose: Pose):
+         self.move_to(obj_pose)
+
+    def move_to_drop_location(self, location: Pose):
+        self.move_to(location)
+
+
+class PickAndPlace():
+
+    def __init__(self):
+        rclpy.init()
+        self.logger = get_logger("pick_place")
+
+        # instantiate MoveItPy instance and get planning component
+        self.moveit = MoveItPy(node_name="pick_place")
+        #self.arm = self.moveit.get_planning_component("lite6")
+        self.logger.info("Pick and Place MoveItPy instance created")
+
+        ### Setup a second node
+        self.node = rclpy.create_node("object_despawner")
+        self.logger.info("created object despawn node!")
+
+        ## Setup necessary pub/sub
+        self.publisher = self.node.create_publisher(PlanningScene, "/planning_scene", 10)
+        self.subscriber = self.node.create_subscription(
+                PlanningScene,
+                "/planning_scene",
+                self.scene_changed_callback,
+                10)
+
+        self.object_queue = SimpleQueue()
+
+        ## Setup necessary parameters
+        ## vacuum gripper
+        self.node.declare_parameter("add_vacuum_gripper", False)
+        self.vacuum_gripper = self.node.get_parameter("add_vacuum_gripper").get_parameter_value().bool_value
+
+        ## robot_ip
+        self.node.declare_parameter("robot_ip", "fake")
+        self.robot_ip = self.node.get_parameter("robot_ip").get_parameter_value().string_value
+
+        self.logger.info(f"CONFIGURATION: vacuum_gripper {self.vacuum_gripper}, robot_ip {self.robot_ip}")
+
+        # finally initialize appropriate robot logic
+        # TODO: check parameters and instantiate appropriate classes
+        if self.vacuum_gripper and self.robot_ip != "fake":
+            self.logger.info(f"this is real arm, with vacuum gripper")
+            srv_cli = self.node.create_client(VacuumGripperCtrl, "/ufactory/set_vacuum_gripper")
+            while not srv_cli.wait_for_service(timeout_sec=1.0):
+                self.logger.info(f"waiting for xarm VacuumGripperCtrl to come up")
+            self.robot_logic = RobotWithVacuumGripper(self.moveit, self.logger, srv_cli)
+        elif self.vacuum_gripper and self.robot_ip == "fake":
+            self.logger.info(f"we have a fake robot, but with a vacuum gripper")
+            self.robot_logic = RobotWithoutGripper(self.moveit, self.logger, "link_eef")
+        else:
+            #we have a physical or fake robot without vacuum gripper
+            self.robot_logic = RobotWithoutGripper(self.moveit, self.logger, "link6")
+
+
+
+    def start_spinning(self):
+        self.thread = threading.Thread(target=self.spin)
+        self.thread.start()
+
+    def spin(self):
+        self.logger.info("sub_thread: start spinning!")
+        rclpy.spin(self.node)
+
+
+    def scene_changed_callback(self, msg: PlanningScene):
+        self.logger.info("scene changed!")
+        for obj in extract_objects_from_scene_message(msg):
+            self.object_queue.put(obj)
 
     def attach(self, obj: CollisionObject) -> AttachedCollisionObject:
         self.logger.info(f"ATTACHING OBJECT ID: {obj.id}")
 
+
+        # TODO change link6 depending on the parameters
         obj.header.frame_id = "link6" #relative to the eef
 
         object_pose = Pose()
@@ -177,48 +293,22 @@ class PickAndPlace():
             #step 2: move to object (TODO Implicit assumption that all objects are boxes..)
             pick_location = copy.copy(obj.pose)
             pick_location.position.z += obj.primitives[0].dimensions[SolidPrimitive.BOX_Z]/2.
-            self.move_to(pick_location)
+            self.robot_logic.move_to_object(pick_location)
+            #self.move_to(pick_location)
 
             #step 3: pickup/attach object
             attached_object = self.attach(obj)
+            self.robot_logic.attach_physical_object(obj)
 
             #step 4: move to drop location
             drop_location.position.z = obj.primitives[0].dimensions[SolidPrimitive.BOX_Z] + 0.01
-            self.move_to(drop_location)
+            #self.move_to(drop_location)
+            self.robot_logic.move_to_drop_location(drop_location)
 
             #step 5: detach
             self.detach(attached_object)
+            self.robot_logic.detach_physical_object()
 
-    def plan_and_execute(
-            self,
-            single_plan_parameters=None,
-            multi_plan_parameters=None,
-            sleep_time=0.0,
-            ):
-        """Helper function to plan and execute a motion."""
-        # plan to goal
-        self.logger.info("Planning trajectory")
-        if multi_plan_parameters is not None:
-            plan_result = self.arm.plan(
-                multi_plan_parameters=multi_plan_parameters
-                )
-        elif single_plan_parameters is not None:
-            plan_result = self.arm.plan(
-                single_plan_parameters=single_plan_parameters
-                )
-        else:
-            plan_result = self.arm.plan()
-
-        # execute the plan
-        if plan_result:
-            self.logger.info("Executing plan")
-            robot_trajectory = plan_result.trajectory
-            self.moveit.execute(robot_trajectory, controllers=[])
-            self.logger.info("Execute finished!")
-        else:
-            self.logger.error("Planning failed")
-
-        time.sleep(sleep_time)
 
 def extract_objects_from_scene_message(scene: PlanningScene) -> list[CollisionObject]:
     result = []
