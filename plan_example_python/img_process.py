b52fb1915b00ec0c372dcd1e2cf3eeaf5907366c
smarios <smarios@jaist.ac.jp>
2025-11-19 15:41:06 +0900

    Simplify ndarray calculation, experiment with erosion

--- plan_example_python/img_process.py
+++ plan_example_python/img_process.py
@@ -15,7 +15,7 @@ from sensor_msgs.msg import PointCloud2, Image
 class ImageProccessor:
 
     def __init__(self, node, logger):
-        self.node = node 
+        self.node = node
         self.logger = logger
         self.count = 0
         self.br = CvBridge()
@@ -24,45 +24,37 @@ class ImageProccessor:
         self.node.create_subscription(
                 PointCloud2, 
                 "/camera/depth_registered/points", 
-                #"/camera/depth/points", 
                 self.sub_callback, 10)
-        #self.node.create_subscription(
-        #        Image,
-        #        "/camera/color/image_raw",
-        #        self.sub_color, 10)
 
 
     def sub_callback(self, msg: PointCloud2):
+        self.count = (self.count + 1) % 10
+
         self.logger.info("I received a message!") 
         self.logger.info(f"msg info: \n  isbigendian? {msg.is_bigendian}, point_step: {msg.point_step}")
         self.logger.info(f"row_step? {msg.row_step}, h/w: {msg.height}/{msg.width}")
         for field in msg.fields:
             self.logger.info(f"field info: {field.name}, {field.offset}, {field.datatype}, {field.count}")
         self.logger.info(f"data len: {len(msg.data)}, is_dense? {msg.is_dense}")
-        ndpc = read_points(msg, reshape_organized_cloud=True)
-        self.logger.info(f"ndpc.shape is: {ndpc.shape}, dtype: {ndpc.dtype}")
-        self.logger.info("read a point cloud!")
 
-        ndpc_rgb = np.asfortranarray(ndpc['rgb']).reshape(720,1280).view((np.uint8, 4))
+        ndpc = read_points(msg, reshape_organized_cloud=True)
+        self.logger.info(f"ndpc.shape is: {ndpc.shape}, dtype: {ndpc.dtype}, flags are: {ndpc.flags}")
+
+        #TODO: why do we have to reshape the array? row/column-major order
+        # someone is lying to me
+        ndpc_rgb = ndpc['rgb'].reshape(ndpc.shape[1], ndpc.shape[0]).view((np.uint8, 4))
 
         self.logger.info(f"ndpc_rgb.shape is: {ndpc_rgb.shape}, dtype: {ndpc_rgb.dtype}")
 
+        mask = self.back.apply(ndpc_rgb)
+
+        kernel = np.ones((5, 5), np.uint8)
+        mask_er = cv2.erode(mask, kernel, 1)
+
         if self.count == 0:
             cv2.imwrite("/tmp/depth.bmp", ndpc_rgb)
-
-
-    def sub_color(self, msg: Image):
-        self.count = (self.count + 1) % 30 
-        self.logger.info("I received an Image!") 
-        self.logger.info(f"info w/h/e: {msg.width}, {msg.height}, {msg.encoding}")
-        if self.count == 0:
-            cvimg = self.br.imgmsg_to_cv2(msg, "rgb8")
-            self.logger.info(f"  cvimg shape: {cvimg.shape} dtype: {cvimg.dtype}")
-            mask = self.back.apply(cvimg)
-            cv2.imwrite("test.jpg", cvimg)
-            cv2.imwrite("mask.jpg", mask)
-        #raw = np.frombuffer(msg.data, dtype=np.uint8)#.reshape(1280, 720, 3)
-        #cv2_read = cv2.imdecode(raw, cv2.IMREAD_ANYCOLOR)#.reshape(1280, 720, 3)
+            cv2.imwrite("/tmp/mask.bmp", mask)
+            cv2.imwrite("/tmp/mask_er.bmp", mask_er)
 
 
 
